d0b5117e88474b1ff4340ace5a655b8ed4fbb7e8
bzenz <benito@benitozenz.de>
2024-07-28 22:19:31 +0200

    Install st

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 # Install xorg
-echo "### Installing utilities"
+echo "### Installing xorg"
 sudo pacman -S xorg xorg-xev xorg-xinit --noconfirm || exit 1
 
 # Install utils
 echo "### Installing utilities"
-sudo pacman -S git man pacman-contrib arandr vim neovim htop neofetch dmenu ranger nitrogen dunst telegram-desktop spectacle mpv feh nodejs npm unzip ripgrep fd --noconfirm || exit 1
+sudo pacman -S git man pacman-contrib arandr vim neovim htop neofetch dmenu ranger nitrogen dunst telegram-desktop spectacle mpv feh nodejs npm unzip ripgrep fd xclip --noconfirm || exit 1
 
 # Install utils
 echo "### Installing language servers"
@@ -14,7 +14,7 @@ sudo pacman -S llvm llvm-libs lua-language-server ccl typescript-language-server
 
 # Install fonts
 echo "### Installing fonts"
-sudo pacman -S ttf-font-awesome otf-font-awesome ttf-firacode-nerd --noconfirm || exit 1
+sudo pacman -S fontconfig ttf-font-awesome otf-font-awesome ttf-firacode-nerd --noconfirm || exit 1
 
 # Install starship
 echo "### Installing starship"
@@ -63,5 +63,14 @@ if [ ! -d $HOME/.build/dwmblocks ]; then
 	git clone git@github.com:vegedy/dwmblocks.git || exit 1
 	cd dwmblocks || exit 1
 	./rebuild.sh || exit 1
+fi
+
+# Install dwmblocks
+echo "### Installing st"
+if [ ! -d $HOME/.build/st ]; then
+	cd ~/.build || exit 1
+  git clone https://github.com/LukeSmithxyz/st || exit 1
+  cd st || exit 1
+  sudo make install || exit 1
 fi
 
