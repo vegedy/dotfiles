a3bc54812af907af0af0d16090f7064e2011a4d3
bzenz <benito@benitozenz.de>
2024-08-13 08:22:16 +0200

    Use zathura

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -6,7 +6,7 @@ sudo pacman -S xorg xorg-xev xorg-xinit --noconfirm || exit 1
 
 # Install utils
 echo "### Installing utilities"
-sudo pacman -S git man base-devel pacman-contrib arandr vim neovim htop fastfetch dmenu ranger nitrogen dunst telegram-desktop spectacle mpv feh nodejs npm unzip ripgrep fd xclip python-pywal --noconfirm || exit 1
+sudo pacman -S git man base-devel pacman-contrib arandr vim neovim htop fastfetch dmenu ranger nitrogen dunst telegram-desktop spectacle mpv feh nodejs npm unzip ripgrep fd xclip python-pywal zathura zathura-pdf-mupdf --noconfirm || exit 1
 
 # Install utils
 echo "### Installing language servers"
@@ -84,6 +84,10 @@ fi
 
 # Setup language
 echo "### Setup language"
-sudo sed -i '/^#de_DE.UTF-8 UTF-8/s/^#//' /etc/locale.gen # Enable de_DE.UTF-8 UTF-8
-sudo locale-gen
+sudo sed -i '/^#de_DE.UTF-8 UTF-8/s/^#//' /etc/locale.gen || exit 1 # Enable de_DE.UTF-8 UTF-8
+sudo locale-gen || exit 1
+
+# Setup default programs
+echo "### Setup default programs" || exit 1
+xdg-mime default zathura.desktop application/pdf || exit 1
 
